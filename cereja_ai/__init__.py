49bdd71540c6e69d21e9bf827b1504320b1ba18a
Joab Leite <leitejoab@gmail.com>
2023-03-13 14:55:42 -0300

    publish: deploy first version

--- cereja_ai/__init__.py
+++ cereja_ai/__init__.py
@@ -1,4 +1,5 @@
-from cereja.utils import get_version_pep440_compliant
+# TODO: fix dynamic version for toml.
+# from cereja.utils import get_version_pep440_compliant
 
 VERSION = "0.0.1.final.0"
-__version__ = get_version_pep440_compliant(VERSION)
+__version__ = VERSION[:5]
