c0012d0663378f93db092cb5addb1f4ad5cfbe26
Joab Leite <leitejoab@gmail.com>
2023-03-14 05:22:24 -0300

    improves: improve extractor

--- cereja_ai/__init__.py
+++ cereja_ai/__init__.py
@@ -1,4 +1,5 @@
 from cereja.utils import get_version_pep440_compliant
+from . import vision
 
-VERSION = "0.0.2.final.0"
+VERSION = "0.0.3.final.0"
 __version__ = get_version_pep440_compliant(VERSION)
