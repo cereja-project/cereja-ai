5c9a4d0a42a95b1e81145dfd0fd428c385a85f5e
Joab Leite <leitejoab@gmail.com>
2023-04-03 04:23:30 -0300

    fixes: build toml fixed

--- cereja_ai/__init__.py
+++ cereja_ai/__init__.py
@@ -1,7 +1,12 @@
 from cereja.utils import get_version_pep440_compliant
-from . import vision
-from . import losses
-from . import metrics
 
-VERSION = "0.0.4.final.0"
+# TODO: skip on build. Solve this
+try:
+    from . import vision
+    from . import losses
+    from . import metrics
+except:
+    pass
+
+VERSION = "0.0.5.final.0"
 __version__ = get_version_pep440_compliant(VERSION)
